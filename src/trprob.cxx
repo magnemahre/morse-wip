b525f1482031e3ff052ca69b5217cc89f905450a
Magne Mæhre <magne.mahre@gmail.com>
2014-04-19 22:12:21 +0200

    First cleanup of trprob.cxx

--- src/trprob.cxx
+++ src/trprob.cxx
@@ -25,13 +25,8 @@
 
 int morse::trprob_(integer *ip, integer *lambda, real *dur, integer *ilrate, real *p)
 {
-    static integer i, k, n;
+    
     static real pin[30];
-    static integer kelm;
-    static real psum, ptrx;
-    static integer ielem, irate;
-
-
 
 /* 		THIS SUBROUTINE COMPUTES THE TRANSITION PROBABILITY */
 /* 		FROM SAVED PATH IP TO EACH STATE N AND STORES THE */
@@ -54,23 +49,23 @@ int morse::trprob_(integer *ip, integer *lambda, real *dur, integer *ilrate, rea
 
     /* Function Body */
     if (*lambda == 0) {
-		for (n = 1; n <= 30; ++n) {
+		for (int n = 1; n <= 30; ++n) {
 			p[*ip + n * 25] = 0.f;
 		}
 		return 0;
     }
 
-    ielem = ilami[ielmst[*lambda - 1] - 1];
+    integer ielem = ilami[ielmst[*lambda - 1] - 1];
 /* 	COMPUTE KEYSTATE TRANSITION PROBABILITY: */
-    ptrx = xtrans_(&ielem, dur, ilrate);
+    real ptrx = xtrans_(&ielem, dur, ilrate);
 
 /* 	FOR EACH STATE, COMPUTE STATE TRANSITION PROBABILITY: */
-    psum = 0.f;
-    for (k = 1; k <= 6; ++k) {
-		for (i = 1; i <= 5; ++i) {
-			n = (i - 1) * 6 + k;
-			kelm = k;
-			irate = i;
+    real psum = 0.f;
+    for (int k = 1; k <= 6; ++k) {
+		for (int i = 1; i <= 5; ++i) {
+			integer n = (i - 1) * 6 + k;
+			integer kelm = k;
+			integer irate = i;
 			ptrans_(&kelm, &irate, lambda, ilrate, &ptrx, &psum, pin, &n);
 		}
     }
@@ -79,11 +74,10 @@ int morse::trprob_(integer *ip, integer *lambda, real *dur, integer *ilrate, rea
     	return 0;
     }
 
-    for (n = 1; n <= 30; ++n) {
+    for (int n = 1; n <= 30; ++n) {
 		p[*ip + n * 25] = pin[n - 1] / psum;
     }
 
-L200:
     return 0;
 } /* trprob_ */
 
